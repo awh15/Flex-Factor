014af830c34cfdf232b69896ca7a8668911dcc73
DaaboulMoe <mad64@mail.aub.edu>
2024-04-21 21:24:28 +0300

    User role pages

---
+++ frontend/src/features/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Dashboard = () => {
+    return (
+      <div className="widget" style={{ marginTop: '100px' }}>
+        <h2>Dashboard Widget</h2>
+        <p>This is the Dashboard widget content.</p>
+      </div>
+    );
+  }
+
+
+export default Dashboard;
---
+++ frontend/src/features/profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+      <div className="widget" style={{ marginTop: '100px' }}>
+        <h2>Profile Widget</h2>
+        <p>This is the Profile widget content.</p>
+      </div>
+    );
+  }
+
+
+export default Profile;
